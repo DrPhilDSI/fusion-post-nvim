47a7a6762fab7e07838b79cd3a11218bbda9a2e2
Phil Butterworth <109159255+DrPhilDSI@users.noreply.github.com>
2025-02-10 11:19:09 -0800

    fixed line split

--- lua/fusion_post/insert_boiler.lua
+++ lua/fusion_post/insert_boiler.lua
@@ -6,10 +6,7 @@ function M.insert_boiler_plate(selected_file)
 		local content = file:read("*a")
 		file:close()
 
-		local lines = {}
-		for line in content:gmatch("[^\r\n]+") do
-			table.insert(lines, line)
-		end
+		local lines = vim.split(content, "\n", true)
 
 		vim.api.nvim_put(lines, "l", true, true)
 	end
